d4258fc1677bee6298447d6eb1aa79142e7e0cce
Roger Myung <rmyung@gmail.com>
2026-01-08 11:25:47 -0800

    rep-on off

--- bin/bootstrap.js
+++ bin/bootstrap.js
@@ -10,7 +10,9 @@ export async function main(ns) {
         { remote: "lib/network.js", local: "/lib/network.js" },
         { remote: "lib/analyze.js", local: "/lib/analyze.js" },
         { remote: "bin/upgrade-servers.js", local: "/bin/upgrade-servers.js" },
-        { remote: "core/orchestrator.js", local: "/core/orchestrator.js" }
+        { remote: "core/orchestrator.js", local: "/core/orchestrator.js" },
+        { remote: "bin/rep-on.js", local: "/bin/rep-on.js" },
+        { remote: "bin/rep-off.js", local: "/bin/rep-off.js" }
     ];
 
     ns.tprint("🚀 Starting Bootstrap Process...");
